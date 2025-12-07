c03af6e7cf388a8b2721001cdbce3d7e122fcb62
Mr.Vit <vitalijdemchenko@MacBook-Air-Vitalij.local>
2025-12-07 17:13:49 +0200

    change 2

--- file1.py
+++ file1.py
@@ -1,1 +1,2 @@
 print("DDDD")
+print("Test")
---
+++ main.py
@@ -0,0 +1,6 @@
+from masks import get_mask_account, get_mask_card_number
+
+print(get_mask_card_number("1234567890123456"))
+print(get_mask_account("12345678901234567890"))
+
+
