c75f5689342f1f5fc27737ae0429cc01f2c0f980
Mr.Vit <mr.white.vw@gmail.com>
2025-12-20 20:11:03 +0200

    Добавлены описания функций

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(list_of_dict: list[dict], state='EXECUTED') -> list:
+    """Функция получает на вход список словарей и возвращает список по условию значение state"""
     result_list = list()
     for element in list_of_dict:
         for key, value in element.items():
@@ -8,4 +9,5 @@ def filter_by_state(list_of_dict: list[dict], state='EXECUTED') -> list:
 
 
 def sort_by_date(list_of_dict: list[dict], need_reverse=False) -> list:
+    """Функция получает на вход список возвращает отсортированный по дате список"""
     return sorted(list_of_dict, key=lambda s_date: s_date['date'], reverse=need_reverse)
