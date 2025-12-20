b7a0cd0bcf734b593868f379e9c06afff8846374
Mr.Vit <mr.white.vw@gmail.com>
2025-12-20 20:07:45 +0200

    Добавлена функция сортировки

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,11 @@
-def filter_by_state(list_d: list[dict], state='EXECUTED'):
+def filter_by_state(list_of_dict: list[dict], state='EXECUTED') -> list:
     result_list = list()
-    for element in list_d:
+    for element in list_of_dict:
         for key, value in element.items():
             if (key == 'state') & (value == state):
                 result_list.append(element)
     return result_list
+
+
+def sort_by_date(list_of_dict: list[dict], need_reverse=False) -> list:
+    return sorted(list_of_dict, key=lambda s_date: s_date['date'], reverse=need_reverse)
