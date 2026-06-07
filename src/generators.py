00bd8f13bcdefa4f0e9d4e3ac5ee02df3284b3ab
Mr.Vit <mr.white.vw@gmail.com>
2026-06-07 16:24:54 +0200

    Добавлены генераторы

--- src/generators.py
+++ src/generators.py
@@ -1,16 +1,19 @@
-def filter_by_currency(transactions: list, currency: str) -> list:
+def filter_by_currency(transactions: list, currency: str):
+    """Функция принимает список транзакций и код валюты, а возвращает итератор с транзакциями в указанной валюте."""
+    filter_list = filter(lambda s: s["operationAmount"]["currency"]["code"] == currency, transactions, )
 
-    filter_list = filter(lambda s: s.operationAmount.currency.code == currency ,transactions)
     for next_elem in filter_list:
         yield next_elem
 
 
 def transaction_descriptions(transactions: list) -> str:
+    """Функция принимает список транзакций и по очереди возвращает описание каждой операции."""
     for next_elem in transactions:
         yield next_elem.descriptions
 
 
-def card_number_generator(fist_num:int , last_num:int) -> str:
+def card_number_generator(fist_num: int, last_num: int) -> str:
+    """Функция `card_number_generator` принимает начальное и конечное значения диапазона и генерирует номера карт в формате `XXXX XXXX XXXX XXXX`"""
     card_list = [x for x in range(fist_num, last_num)]
     for num in card_list:
         str_num = "0000000000000000"+str(num)
